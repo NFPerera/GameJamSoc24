9fc9724dfd685a5feedbd502eede36ea0324295d
IanDalton <daltioan@gmail.com>
2024-10-18 17:33:34 -0300

    weapon modes

---
+++ GameJamSoc2024/Assets/Main/Scripts/PlayerFPS/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Target")){
+            print("hit "+collision.gameObject.name+ " !");
+            Destroy(collision.gameObject);
+        }
+        if(collision.gameObject.CompareTag("Wall")){
+            print("hit wall");
+            Destroy(gameObject);
+        }
+    } 
+}
--- GameJamSoc2024/Assets/Main/Scripts/PlayerFPS/Weapon.cs
+++ GameJamSoc2024/Assets/Main/Scripts/PlayerFPS/Weapon.cs
@@ -1,33 +1,122 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public Camera playerCamera;
 
+    // Shooting
+    public bool isShooting,readyToShoot;
+    bool allowReset =true;
+    public float shootingDelay = 2f;
+
+    // Burst
+    public int bulletsPerBurst =3;
+    public int burstBulletsLeft;
+
+
+    // Spread
+    public float spreadIntentensity;
+    
+
+    // Bullet
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public float bulletVelocity = 30;
     public float bulletPrefabLifeTime = 3f;
 
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
+    public ShootingMode currentShootingMode;
+
+    private void Awake()
+    {
+        readyToShoot = true;
+        burstBulletsLeft = bulletsPerBurst;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)){
+        if(currentShootingMode == ShootingMode.Auto){
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else if (currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        if (readyToShoot && isShooting)
+        {
+            burstBulletsLeft = bulletsPerBurst;
             FireWeapon();
         }
+        
     }
 
     private void FireWeapon()
     {
-        
+        readyToShoot = false;
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        // Point at dir
+        bullet.transform.forward = shootingDirection;
+
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.AddForce(bulletSpawn.forward.normalized*bulletVelocity,ForceMode.Impulse);
         
         StartCoroutine(DestroyBulletAfterTime(bullet,bulletPrefabLifeTime));
 
+        if (allowReset){
+            Invoke("ResetShot",shootingDelay);
+            allowReset = false;
+        
+        }
+
+        // Burst Mode
+        if (currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1)
+        {
+            burstBulletsLeft--;
+            
+            Invoke("FireWeapon",shootingDelay);
+        }
+
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+
+    private Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
+        RaycastHit hit;
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray,out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+
+        float x = UnityEngine.Random.Range(-spreadIntentensity,spreadIntentensity);
+        float y = UnityEngine.Random.Range(-spreadIntentensity,spreadIntentensity);
+
+        return direction + new Vector3(x,y,0);
+        
     }
 
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float bulletPrefabLifeTime)
