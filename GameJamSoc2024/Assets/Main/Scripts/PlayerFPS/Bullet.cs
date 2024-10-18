ac77629f45096dd351e49af2070fed9c101ca975
IanDalton <daltioan@gmail.com>
2024-10-18 20:03:01 -0300

    assetpack and bullet holes

--- GameJamSoc2024/Assets/Main/Scripts/PlayerFPS/Bullet.cs
+++ GameJamSoc2024/Assets/Main/Scripts/PlayerFPS/Bullet.cs
@@ -6,13 +6,37 @@ public class Bullet : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Target")){
-            print("hit "+collision.gameObject.name+ " !");
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            print("hit " + collision.gameObject.name + " !");
+            CreateBulletImpactEffect(collision);
             Destroy(collision.gameObject);
         }
-        if(collision.gameObject.CompareTag("Wall")){
+        if (collision.gameObject.CompareTag("Wall"))
+        {
             print("hit wall");
+            CreateBulletImpactEffect(collision);
             Destroy(gameObject);
         }
-    } 
+    }
+
+    void CreateBulletImpactEffect(Collision collision)
+    {
+
+        ContactPoint contact = collision.contacts[0];
+
+        // Adjust the position slightly along the normal to avoid texture overlap
+        Vector3 adjustedPosition = contact.point + contact.normal * 0.01f;
+
+        GameObject hole = Instantiate(
+            GlobalReferences.Instance.bulletImpactEffect,
+            adjustedPosition,
+            Quaternion.LookRotation(contact.normal) * Quaternion.Euler(0, 180, 0)
+        );
+
+
+        hole.transform.SetParent(collision.gameObject.transform);
+
+
+    }
 }
