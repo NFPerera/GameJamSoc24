e205468026be1077c2f579b37e672e4993ac7376
NFPerera <niko22602@gmail.com>
2024-10-19 18:23:21 -0300

    a

--- GameJamSoc2024/Assets/Main/Scripts/DevelopmentUtilities/DictionarySerializer.cs
+++ GameJamSoc2024/Assets/Main/Scripts/DevelopmentUtilities/DictionarySerializer.cs
@@ -5,6 +5,5 @@ namespace Main.Scripts.DevelopmentUtilities
     public class DictionarySerializer
     {
         public List<ulong> keys = new List<ulong>();
-        public List<MasterManager.RoomData> values = new List<MasterManager.RoomData>();
     }
 }
