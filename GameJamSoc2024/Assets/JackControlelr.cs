ef54f695fad109f99d82ca059244113fbc88b9ee
NFPerera <niko22602@gmail.com>
2024-10-20 14:08:47 -0300

    fffr

---
+++ GameJamSoc2024/Assets/JackControlelr.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JackControlelr : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("asdf");
+        if (other.gameObject.CompareTag("Bullet"))
+            SceneManager.LoadScene("YouWin");
+    }
+}
